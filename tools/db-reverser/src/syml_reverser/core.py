144dede4f07ff8b683ef496e0d862201f49ccb85
Zenderg <danil54658798@gmail.com>
2021-04-04 21:20:27 +0300

    add select schema and views

--- tools/db-reverser/src/syml_reverser/core.py
+++ tools/db-reverser/src/syml_reverser/core.py
@@ -1,18 +1,17 @@
 from os import getenv
 
-import yaml
 from sqlalchemy import MetaData, create_engine
 
 CONNECTION_URI = getenv("DB_REVERSER_CONNECTION_URI", "postgresql://postgres:password@localhost:5432/")
 
 
 class DBReverser:
-    def __init__(self, connection_uri=None, engine=None):
+    def __init__(self, connection_uri=None, engine=None, schema="public", views=False):
         self.connection_uri = connection_uri
         self.engine = engine or create_engine(self.connection_uri)
-        self.metadata = MetaData()
+        self.metadata = MetaData(self.engine, schema=schema)
 
-        self.metadata.reflect(engine)
+        self.metadata.reflect(views=views)
 
     def get_tables(self):
         return self.metadata.tables.values()
@@ -26,27 +25,16 @@ class DBReverser:
             for column in table.columns.values():
                 column_info = {
                     'name': str(column.name),
-                    'type': str(column.type)
+                    'type': str(column.type),
+                    'primary': bool(column.primary_key)
                 }
-
-                if bool(column.primary_key):
-                    column_info['primary'] = True
 
                 tables[str(table.name)].append(column_info)
 
         return tables
-
-    def generate_yaml(self, path='tables.yaml'):
-        yaml_dict = {
-            'tables': self.get_transformed_tables()
-        }
-
-        with open(path, 'w') as file:
-            yaml.dump(yaml_dict, file)
 
 
 if __name__ == '__main__':
     engine = create_engine(CONNECTION_URI)
 
     obj = DBReverser(engine=engine)
-    obj.generate_yaml()
