59c25943d7f24aebaa98ba2ffbfe9dc1ec4d6b1b
Zenderg <danil54658798@gmail.com>
2021-04-04 20:28:46 +0300

    rewrite reverser to class structure

--- tools/db-reverser/src/syml_reverser/core.py
+++ tools/db-reverser/src/syml_reverser/core.py
@@ -1,26 +1,52 @@
 from os import getenv
 
+import yaml
 from sqlalchemy import MetaData, create_engine
 
 CONNECTION_URI = getenv("DB_REVERSER_CONNECTION_URI", "postgresql://postgres:password@localhost:5432/")
 
 
-def main():
-    engine = create_engine(CONNECTION_URI)
-    metadata = MetaData()
-    metadata.reflect(engine)
+class DBReverser:
+    def __init__(self, connection_uri=None, engine=None):
+        self.connection_uri = connection_uri
+        self.engine = engine or create_engine(self.connection_uri)
+        self.metadata = MetaData()
 
-    if len(metadata.tables) == 0:
-        print("Tables not found *o*")
-    else:
-        print(f'{len(metadata.tables)} tables was detected!\n')
+        self.metadata.reflect(engine)
 
-    for table in metadata.tables.values():
-        print(table.name)
-        for column in table.columns.values():
-            print(f'  {"*" if column.primary_key else ""}{column.name} - {None if column.type._isnull else column.type}')
-        print("------------------------")
+    def get_tables(self):
+        return self.metadata.tables.values()
+
+    def get_transformed_tables(self):
+        tables = {}
+
+        for table in self.get_tables():
+            tables[str(table.name)] = []
+
+            for column in table.columns.values():
+                column_info = {
+                    'name': str(column.name),
+                    'type': str(column.type)
+                }
+
+                if bool(column.primary_key):
+                    column_info['primary'] = True
+
+                tables[str(table.name)].append(column_info)
+
+        return tables
+
+    def generate_yaml(self, path='tables.yaml'):
+        yaml_dict = {
+            'tables': self.get_transformed_tables()
+        }
+
+        with open(path, 'w') as file:
+            yaml.dump(yaml_dict, file)
 
 
 if __name__ == '__main__':
-    main()
+    engine = create_engine(CONNECTION_URI)
+
+    obj = DBReverser(engine=engine)
+    obj.generate_yaml()
