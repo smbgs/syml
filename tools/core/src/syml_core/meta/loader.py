351d61bd57bde3c7c1161dc15eda95de61097046
Alexandr Chichenin <admin@somebugs.com>
2021-04-17 03:54:04 +0300

    Codegen ideas (also affects how CLI is implemented, moving out of "fire" package probably)

--- tools/core/src/syml_core/meta/loader.py
+++ tools/core/src/syml_core/meta/loader.py
@@ -1,8 +1,10 @@
+import copy
 import typing
 from dataclasses import dataclass
 from pathlib import Path
-
 import yaml
+from marshmallow_dataclass import class_schema
+from mergedeep import merge
 
 
 @dataclass
@@ -12,50 +14,61 @@ class MetaSpec:
 
 
 @dataclass
+class ActionSpec:
+    name: str
+    args: typing.Dict[str, str]
+
+
+@dataclass
 class SpecProvidesItemSpec:
     interface: typing.Optional[str]
+    actions: typing.List[ActionSpec]
 
 
 @dataclass
 class SpecRequiresItemSpec:
     runtime: typing.Optional[str]
     service: typing.Optional[str]
+    tool: typing.Optional[str]
 
 
 @dataclass
 class SpecSpec:
     provides: typing.List[SpecProvidesItemSpec]
     requires: typing.List[SpecRequiresItemSpec]
-
-
-@dataclass
-class Service:
-    name: str
+    overrides: typing.Optional[typing.Dict[str, dict]]
 
 
 class MetaLoader:
-
-    body: dict
+    _body: dict
 
     apiVersion: str
-
     meta: MetaSpec
     spec: SpecSpec
 
-    services: typing.Dict[str, Service]
-
-    def __init__(self, path: Path):
+    def __init__(self, body: dict):
         """
         Loads the meta manifest from path
         :param path:
         """
+        self._body = body
+
+        self.apiVersion = self._body.get('apiSpec')
+
+        self.meta = class_schema(MetaSpec)().load(self._body.get('meta'))
+        self.spec = class_schema(SpecSpec)().load(self._body.get('spec'))
+
+    @classmethod
+    def from_path(cls, path: Path):
         # TODO: make this work not only with local files, possibly urllib?
         with open(path, 'r') as def_file:
-            self.body = yaml.load(def_file, Loader=yaml.SafeLoader)
-            self.apiVersion = self.body.get('apiSpec')
-            self.meta = MetaSpec(**self.body.get('meta'))
-            self.spec = SpecSpec(**self.body.get('spec'))
+            return cls(yaml.load(def_file, Loader=yaml.SafeLoader))
 
-            for requires in self.spec.requires:
-                if requires.service:
-                    self.services[requires.service] = Service(name=requires.service)
+    def with_override(self, override_name: str):
+        body = copy.deepcopy(self._body)
+        if self.spec.overrides:
+            override = self.spec.overrides.get(override_name)
+            # TODO: handle missing override profile
+            if override:
+                merge(body, override)
+        return MetaLoader(body)
